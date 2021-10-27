965db99797a4b7bf74212066c48c74a3e0d39553
LyeBlueCode <lyeblue10@hotmail.com>
2021-10-27 13:05:21 +0100

    Re- Prueba 1

---
+++ src/proyect_jdb_gui/Proyect_JDB_GUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyect_jdb_gui;
+
+/**
+ *
+ * @author Leslie J. HL
+ */
+public class Proyect_JDB_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // re- prueba
+    }
+    
+}
