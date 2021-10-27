0201ac42c6553ec7a7e033fe43be7aeda0b651fb
gabri <gabri@DESKTOP-EFEVI04>
2021-10-27 12:47:17 +0100

    ESTE CAMBIO

--- src/proyecto_jdb_gui/Proyecto_JDB_GUI.java
+++ src/proyecto_jdb_gui/Proyecto_JDB_GUI.java
@@ -16,6 +16,7 @@ public class Proyecto_JDB_GUI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
